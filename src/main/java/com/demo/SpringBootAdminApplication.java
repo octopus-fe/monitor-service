2abbe97cb0f8775ca67c57fb64f5f5c06507f026
Joe Zou <yixian.zou@gmail.com>
2018-04-14 23:29:50 +0800

    add security

--- src/main/java/com/demo/SpringBootAdminApplication.java
+++ src/main/java/com/demo/SpringBootAdminApplication.java
@@ -1,9 +1,14 @@
 package com.demo;
 
+import de.codecentric.boot.admin.config.AdminServerProperties;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 @Configuration
 @EnableAutoConfiguration
